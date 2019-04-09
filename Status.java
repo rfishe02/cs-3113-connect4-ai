bbf6cff58dbd73ae8b11cb1bcac45abd530a86e7
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-04-09 14:05:44 -0500

    Add files via upload

--- Status.java
+++ Status.java
@@ -142,6 +142,7 @@ public class Status {
   // Just prints the board.
 
   public static void printBoard(char[][] board) {
+    System.out.println(" "+1+"   "+2+"   "+3+"   "+4+"   "+5+"   "+6+"   "+7);
     for(int row = 0; row < board.length; row++) {
       for(int col = 0; col < board[0].length; col++) {
         System.out.print("["+board[row][col]+"] ");
